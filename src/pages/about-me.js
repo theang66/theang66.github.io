15dc0808718dbcf06b45b9dce5b262d76b1ac22c
theang66 <anguyen1@macalester.edu>
2019-06-28 23:42:52 -0500

    Add links, email and resume

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import Pdf from '../documents/AnhNguyen.pdf';
 
 import SEO from "../components/seo"
 
@@ -7,6 +8,13 @@ const AboutMe = () => (
   <div className="tc">
     <SEO title="About Me" />
     <h1>Anh Nguyen</h1>
+    <p>I am into web development and user experience design!</p>
+    <ul className="list ph3 ph5-ns pv4">
+      <li><a href="https://github.com/theang66" target="_blank">GitHub</a></li>
+      <li><a href="https://www.linkedin.com/in/thuyanhng/" target="_blank">LinkedIn</a></li>
+      <li><a href={Pdf} target="_blank">Resume</a></li>
+      <li><a href="mailto:anguyen1@macalester.edu">Email</a></li>
+    </ul>
     <Link to="/">Homepage</Link>
   </div>
 )
